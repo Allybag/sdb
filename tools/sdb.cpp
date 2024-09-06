89695395a68c8c6da897d691d658edf3f25d7f27
Ally <bagkingally@gmail.com>
2024-09-05 20:39:28 -0400

    Chapter 6: Allow reading registers on sdb command line

--- tools/sdb.cpp
+++ tools/sdb.cpp
@@ -41,6 +41,124 @@ bool is_prefix(std::string_view str, std::string_view of)
     return std::equal(str.begin(), str.end(), of.begin());
 }
 
+void print_help(const std::vector<std::string>& args)
+{
+    if (args.size() == 1)
+    {
+        std::println(R"(Available commands:
+            continue - Resume the process
+            register - Commands for operating on registers
+        )");
+    }
+    else if (is_prefix(args[1], "register"))
+    {
+        std::println(R"(Available commands:
+            read
+            read <register>
+            read all
+            write <register> <value>
+        )");
+    }
+    else
+    {
+        std::println("No help available");
+    }
+}
+
+void handle_register_read(sdb::process& process, const std::vector<std::string>& args)
+{
+    auto format = []<typename T>(T t)
+    {
+        if constexpr (std::is_floating_point_v<T>)
+        {
+            return std::format("{}", t);
+        }
+        else if constexpr (std::is_integral_v<T>)
+        {
+            return std::format("{:#0{}x}", t, sizeof(T) * 2 + 2);
+        }
+        else
+        {
+            // With fmtlib this would just be:
+            // return fmt::format("[{:#04x}]", fmt::join(t, ","));
+            std::string result{"["};
+            for (const auto& val: t)
+            {
+                result = std::format("{}{:#04x},", result, static_cast<int>(val));
+            }
+            if (result.size() == 1)
+            {
+                // Empty array
+                return std::string{"[]"};
+            }
+
+            // Dubiously swap the incorrect trailing , to a closing square bracket
+            result[result.size() - 1] = ']';
+            return result;
+        }
+    };
+
+    if (args.size() == 2 || (args.size() == 3 && args[2] == "all"))
+    {
+        for (auto& info: sdb::g_register_infos)
+        {
+            auto should_print = (args.size() == 3 || info.type == sdb::register_type::Gpr) && info.name != "orig_rax";
+            if (!should_print)
+            {
+                continue;
+            }
+
+            auto value = process.get_registers().read(info);
+            std::println("{}:\t{}", info.name, std::visit(format, value));
+        }
+    }
+    else if (args.size() == 3)
+    {
+        try
+        {
+            auto info = sdb::register_info_by_name(args[2]); 
+            auto value = process.get_registers().read(info);
+            std::println("{}:\t{}", info.name, std::visit(format, value));
+        }
+        catch (sdb::error& err)
+        {
+            std::println("No such register {}", args[2]);
+            return;
+        }
+    }
+    else
+    {
+        print_help({"help", "register"});
+    }
+}
+
+void handle_register_write(sdb::process& process, const std::vector<std::string>& args)
+{
+}
+
+void handle_register_command(sdb::process& process, const std::vector<std::string>& args)
+{
+    if (args.size() < 2)
+    {
+        print_help({"help", "register"});
+        return;
+    }
+
+    if (is_prefix(args[1], "read"))
+    {
+        handle_register_read(process, args);
+    }
+    else if (is_prefix(args[1], "write"))
+    {
+        handle_register_write(process, args);
+    }
+    else
+    {
+        print_help({"help", "register"});
+    }
+
+}
+
 std::unique_ptr<sdb::process> attach(int argc, const char** argv)
 {
     if (argc == 3 && argv[1] == std::string_view("-p"))
@@ -83,6 +201,14 @@ void handle_command(std::unique_ptr<sdb::process>& process, std::string_view lin
         process->resume();
         auto reason = process->wait_on_signal();
         print_stop_reason(*process, reason);
+    }
+    else if (is_prefix(command, "help"))
+    {
+        print_help(args);
+    }
+    else if (is_prefix(command, "register"))
+    {
+        handle_register_command(*process, args);
     }
     else
     {
